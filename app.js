00bcde4a5648476c4b379e5a6007bf5e41dda00d
Student <owen.kiteck@stu.barren.kyschools.us>
2022-04-15 10:43:04 -0500

    added js selectors

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 // APIproject JavaScript
 
+// selecting every button
+const two = document.getElementById(`2`)
+const three = document.getElementById(`3`)
+const four = document.getElementById(`4`)
+const five = document.getElementById(`5`)
+const six = document.getElementById(`6`)
+const seven = document.getElementById(`7`)
+const eight = document.getElementById(`8`)
+const nine = document.getElementById(`9`)
+const ten = document.getElementById(`10`)
+const jack = document.getElementById(`j`)
+const queen = document.getElementById(`q`)
+const king = document.getElementById(`k`)
+const ace = document.getElementById(`a`)
+
+const club = document.getElementById(`club`)
+const heart = document.getElementById(`heart`)
+const diamond = document.getElementById(`diamond`)
+const spade = document.getElementById(`spade`)
+
+two.onclick = function name(params) {
+    
+}
+
+
+
+
+
+
 Answer(() => {
     const yes = document.querySelector(`#yes`){
         alert(`Awesome! Thank you for playing. `)
