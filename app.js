efd6275620407731752b395fccdf08f34c8f1d77
Madison <madison.brock03@gmail.com>
2022-04-18 10:24:26 -0500

    Undated Project

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // APIproject JavaScript
 
+let number;
+let suit;
+
 // selecting every button
 const two = document.getElementById(`2`)
 const three = document.getElementById(`3`)
@@ -20,18 +23,33 @@ const heart = document.getElementById(`heart`)
 const diamond = document.getElementById(`diamond`)
 const spade = document.getElementById(`spade`)
 
-two.onclick = function name(params) {
-    
-}
+two.onclick = () => number = `2`;
+three.onclick = () => number = `3`;
+four.onclick = () => number = `4`;
+five.onclick = () => number = `5`;
+six.onclick = () => number = `6`;
+seven.onclick = () => number = `7`;
+eight.onclick = () => number = `8`;
+nine.onclick = () => number = `9`;
+ten.onclick = () => number = `10`;
+jack.onclick = () => number = `J`;
+queen.onclick = () => number = `Q`;
+king.onclick = () => number = `K`;
+ace.onclick = () => number = `A`;
+
+club.onclick = () => suit = `C`;
+heart.onclick = () => suit = `H`;
+diamond.onclick = () => suit = `D`;
+spade.onclick = () => suit = `S`;
 
 
 
 
 
 
-Answer(() => {
-    const yes = document.querySelector(`#yes`){
-        alert(`Awesome! Thank you for playing. `)
+// Answer(() => {
+//     const yes = document.querySelector(`#yes`){
+//         alert(`Awesome! Thank you for playing. `)
 
-}
+// }
 
